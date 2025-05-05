6a3a2500d87073d367c9c800637235e6b25bdd48
Abdoul Ibrahima Samaké <101934271+Abdoul03@users.noreply.github.com>
2025-05-05 16:47:45 +0000

    lister les groupes avec les action

--- src/features/authentification.ts
+++ src/features/authentification.ts
@@ -1,49 +1,41 @@
 import { Utilisateur } from "../model";
-
-import { loadUser,loadGroupe } from "../depenseManager";
+import { loadUser, loadGroupe } from "../depenseManager";
 import inquirer from "inquirer";
 import { gestionCompte } from "./gererCompte";
-import {listerGroupes} from "./listergroupe"
-import {Groupe} from "../model"
-
-=======
-import { loadGroupe, loadUser } from "../depenseManager";
-import inquirer from "inquirer";
-import { gestionCompte } from "./gererCompte";
-import { createGroupe } from "./createGroupe";
-import { group } from "console";
+import { createGroupe } from "../features/createGroupe";
+import { afficherGroupes } from "../features/listergroupe";
+import { Groupe } from "../model";
 
 // let User: Utilisateur[] = loadUser();
-const groupe:Groupe[] = loadGroupe();
 export async function seconnecter() {
-  const loginInfo = await inquirer.prompt([
-    {
-      type: "input",
-      name: "telephone",
-      message: "Votre numéro de téléphone :",
-    },
-    {
-      type: "password",
-      name: "password",
-      message: "Votre mot de passe :",
-    },
-  ]);
-  const users = loadUser();
-  const user = users.find(
-    (u) =>
-      u.telephone === loginInfo.telephone && u.password === loginInfo.password
-  );
+    const loginInfo = await inquirer.prompt([
+        {
+            type: "input",
+            name: "telephone",
+            message: "Votre numéro de téléphone :",
+        },
+        {
+            type: "password",
+            name: "password",
+            message: "Votre mot de passe :",
+        },
+    ]);
+    const users = loadUser();
+    const user = users.find(
+        (u) =>
+            u.telephone === loginInfo.telephone &&
+            u.password === loginInfo.password
+    );
 
-  if (user) {
-    console.log(`Connexion reussie`);
-    await menuUtilisateur(user);
-  } else {
-    console.log("Identifiants incorrects !");
-  }
+    if (user) {
+        console.log(`Connexion reussie`);
+        await menuUtilisateur(user);
+    } else {
+        console.log("Identifiants incorrects !");
+    }
 }
 
 async function menuUtilisateur(user: Utilisateur) {
-
     const { action } = await inquirer.prompt([
         {
             type: "list",
@@ -59,9 +51,10 @@ async function menuUtilisateur(user: Utilisateur) {
     ]);
     switch (action) {
         case "Creer un groupe":
+            await createGroupe(user);
             break;
         case "Voir mes goupes":
-            await listerGroupes(groupe)
+            await afficherGroupes(user);
             break;
         case "Gerer Mon Compte":
             // await gestionCompte(user);
@@ -76,38 +69,4 @@ async function menuUtilisateur(user: Utilisateur) {
             return;
     }
     await menuUtilisateur(user); // Boucle tant que connecté
-
-  const { action } = await inquirer.prompt([
-    {
-      type: "list",
-      name: "action",
-      message: `Bienvenue Sur votre compe ${user.prenom}`,
-      choices: [
-        "Creer un groupe",
-        "Voir mes goupes",
-        "Gerer Mon Compte",
-        "Me deconnecter",
-      ],
-    },
-  ]);
-  switch (action) {
-    case "Creer un groupe":
-      await createGroupe(user);
-      break;
-    case "Voir mes goupes":
-      break;
-    case "Gerer Mon Compte":
-      // await gestionCompte(user);
-      const shouldLogout = await gestionCompte(user);
-      if (shouldLogout) {
-        console.log("Vous êtes maintenant déconnecté.");
-        return; // Quitter le menu utilisateur
-      }
-      break;
-    case "Me deconnecter":
-      console.log("Déconnexion effectuée !");
-      return;
-  }
-  await menuUtilisateur(user); // Boucle tant que connecté
-
 }
--- src/features/listergroupe.ts
+++ src/features/listergroupe.ts
@@ -1,23 +1,64 @@
-import {loadGroupe}  from "../depenseManager"
+import { Groupe, Utilisateur } from "../model";
+import { loadGroupe } from "../depenseManager";
 import inquirer from "inquirer";
-import {Groupe} from "../model"
 
-//afficher du groupe
-const groupes:Groupe[] = loadGroupe();
+export async function afficherGroupes(user: Utilisateur) {
+    const groupes: Groupe[] = loadGroupe();
 
-export async function listerGroupes(groupe : Groupe[] ) : Promise<void> {
-    console.log("Liste des groupes : ");
-    groupes.forEach(groupe => {
-        console.log ('nom: ${groupe.nom}');
-        console.log('membres:${groupe.membres.join(', ')})');
-        
-    });
+    const mesGroupes = groupes.filter((g) => g.membreId?.includes(user.id));
+
+    if (mesGroupes.length === 0) {
+        console.log("Vous n'êtes membre d'aucun groupe.");
+        return null;
+    }
+
+    const { groupeChoisi } = await inquirer.prompt([
+        {
+            type: "list",
+            name: "groupeChoisi",
+            message: "Sélectionnez un groupe :",
+            choices: mesGroupes.map((groupe) => ({
+                name: `${groupe.nom} - ${groupe.description}`,
+                value: groupe.id,
+            })),
+        },
+    ]);
+
+    if (groupeChoisi) {
+        const { action } = await inquirer.prompt([
+            {
+                type: "list",
+                name: "action",
+                message: "Que voulez-vous faire dans ce groupe",
+                choices: [
+                    "Ajouter une depense",
+                    "Voir les membres",
+                    "Ajouter des membres",
+                    "Supprimer um membre",
+                    "Supprimer le groupe",
+                    "Retour",
+                ],
+            },
+        ]);
+        switch (action) {
+            case "Ajouter une depense":
+                console.log("ajouter une depense");
+                break;
+            case "Voir les membres":
+                console.log("voir les depenses");
+                break;
+            case "Ajouter des membres":
+                console.log("Ajouter des membres");
+                break;
+            case "Supprimer um membre":
+                console.log("Supprimer um membre");
+
+                break;
+            case "Supprimer le groupe":
+                console.log("Supprimer le groupe");
+                break;
+            case "Retour":
+                return;
+        }
+    }
 }
- 
-if (groupes.length >0) {
-    listerGroupes (groupes) ;
-
-} else {
-    console.log("Aucun groupe trouvé")
-}
-
